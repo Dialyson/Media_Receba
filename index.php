ac37a67a45efe693474812938ea7f1a81df1c193
Dydson <familiasouza999@gmail.com>
2022-06-06 21:35:38 -0300

    tu

--- index.php
+++ index.php
@@ -1,74 +1,26 @@
-<!doctype html>
 <html lang="pt-BR">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Receba-Nota</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Loca-Car Cariri</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
-
 <body>
-    <div class="container">
-
-        <?php
-        echo "<table class='table'>";
-        echo "<thead>";
-        echo "<tr>";
-        echo "<th scope='col'>ID</th>";
-        echo "<th scope='col'>NOME</th>";
-        echo "<th scope='col'>EMAIL</th>";
-        echo "</tr>";
-        echo "</thead>";
-
-
-        class TableRows extends RecursiveIteratorIterator
-        {
-            function __construct($it)
-            {
-                parent::__construct($it, self::LEAVES_ONLY);
-            }
-
-            function current()
-            {
-                return "<td>" . parent::current() . "</td>";
-            }
-
-            function beginChildren()
-            {
-                echo "<tr>";
-            }
-
-            function endChildren()
-            {
-                echo "</tr>" . "\n";
-            }
-        }
-
-        include "bd.php";
-
-        $conn = connection();
-
-        try {
-            //$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $stmt = $conn->prepare("SELECT * from clientes");
-            $stmt->execute();
-
-            // set the resulting array to associative
-            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-            foreach (new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k => $v) {
-                echo $v;
-            }
-        } catch (PDOException $e) {
-            echo "Error: " . $e->getMessage();
-        }
-        $conn = null;
-        echo "</table>";
-        ?>
-    <div>
-
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  <h1 style="text-align: center; padding-top: 65px; padding-bottom: 50px;">Loca-Car Cariri</h1>
+  <div class="container">
+    <form name="customer" id="customer" method="POST" enctype="multipart/form-data" action="add-customer.php">
+      <div class="form-group">
+        <label for="exampleInputPassword1">Nome Completo</label>
+        <input type="text" class="form-control" id="nome" name="nome">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">E-mail</label>
+        <input type="email" class="form-control" id="email" name="email">
+      </div>
+      <br>
+      <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
-
 </html>
