6e980ce40fec6504c010e912f0a8ee7726ba1698
Dydson <familiasouza999@gmail.com>
2022-06-06 21:41:33 -0300

    123

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 <body>
-  <h1 style="text-align: center; padding-top: 65px; padding-bottom: 50px;">Loca-Car Cariri</h1>
+  <h1 style="text-align: center; padding-top: 65px; padding-bottom: 50px;">Receba Notas</h1>
   <div class="container">
     <form name="customer" id="customer" method="POST" enctype="multipart/form-data" action="add-customer.php">
       <div class="form-group">
@@ -17,9 +17,15 @@
       <div class="form-group">
         <label for="exampleInputPassword1">E-mail</label>
         <input type="email" class="form-control" id="email" name="email">
+        <label for="exampleInputPassword1">Nota 1</label>
+        <input type="text" class="form-control" id="nota_1" name="nota_1">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Nota 2</label>
+        <input type="email" class="form-control" id="nota_2" name="nota_2">
       </div>
       <br>
-      <button type="submit" class="btn btn-primary">Cadastrar</button>
+      <button type="submit" class="btn btn-primary">Inserir</button>
     </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
