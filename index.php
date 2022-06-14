b399fcf1205f7e082452ba31016931c9ee06904a
Dydson <84354256+Dialyson@users.noreply.github.com>
2022-06-14 15:02:37 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,38 @@
-<html lang="pt-BR">
-
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Receba Notas</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar Notas</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
-  <h1 style="text-align: center; padding-top: 65px; padding-bottom: 50px;">Receba Notas</h1>
-  <div class="container">
-    <form name="notas" id="notas" method="POST" enctype="multipart/form-data" action="cadastrar.php">
-      <div class="form-group">
-        <label for="exampleInputPassword1">Aluno</label>
-        <input type="text" class="form-control" id="nome" name="nome">
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Disciplina</label>
-        <input type="text" class="form-control" id="disciplina" name="disciplina">
-        <label for="exampleInputPassword1">Nota 1</label>
-        <input type="number" class="form-control" id="nota_1" name="nota_1">
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Nota 2</label>
-        <input type="number" class="form-control" id="nota_2" name="nota_2">
-      </div>
-      <br>
-      <button type="submit" class="btn btn-primary">Inserir</button>
-    </form>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <h2>Cadastre aqui sua nota</h2>
+   
+    <div class="container">
+        <form name="notas" id="notas" method="POST" enctype="multipart/form-data" action="cadastro.php">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Aluno</label>
+                <input type="text" class="form-control" name="nome" id="nome" aria-describedby="nomeHelp" placeholder="Nome Completo">
+            </div>
+             <div class="form-group">
+                <label for="exampleInputPassword1">Disciplina</label>
+                <input type="text" class="form-control" name="disciplina" id="disciplina" placeholder="Disciplina">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Nota 01</label>
+                <input type="number" class="form-control" name="nota1" id="nota1" placeholder="Nota">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Nota 02</label>
+                <input type="number" class="form-control" name="nota2" id="nota2" placeholder="Nota">
+            </div>
+        
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+    </div>  
+    
+    
 </body>
 </html>
